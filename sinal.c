956733579eab745c7bb8664fe65cce3d9479c4f8
aquila0101 <162712945+aquila0101@users.noreply.github.com>
2024-09-03 21:42:06 +0000

    Create sinal.c

---
+++ sinal.c
@@ -0,0 +1,26 @@
+#Saida Digital
+int vermelho = 13
+int amarelo  = 12
+int verde    = 11
+
+#Entrada Digital
+int vermelhob = 3
+int amarelob  = 4 
+int verdeb    = 5
+
+void setup() {
+
+  #Luz Vermelha
+	pinMode(vermelho, OUTPUT);
+	pinMode(vermelhob, IMPUT);
+
+  #luz Amarela
+	pinMode(amarelo, OUTPUT);
+	pinMode(amarelob, IMPUT);
+
+  #Luz Verde
+  pinMode(verde, OUTPUT);
+  pinMode(verdeb, IMPUT);
+
+  
+}
