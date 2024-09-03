62b3c38ee5db529a360f5baa1ed0e1a7ed34fecc
aquila0101 <162712945+aquila0101@users.noreply.github.com>
2024-09-03 22:12:56 +0000

    Update sinal.c

--- sinal.c
+++ sinal.c
@@ -4,23 +4,27 @@ int amarelo  = 12
 int verde    = 11
 
 #Entrada Digital
-int vermelhob = 3
-int amarelob  = 4 
-int verdeb    = 5
+int botao = 3
+int cartao = 6
+
 
 void setup() {
 
   #Luz Vermelha
 	pinMode(vermelho, OUTPUT);
-	pinMode(vermelhob, IMPUT);
+	pinMode(cartao & botao, INPUT);
 
   #luz Amarela
-	pinMode(amarelo, OUTPUT);
-	pinMode(amarelob, IMPUT);
+       pinMode(amarelo, OUTPUT);
+       pinMode(cartao & botao, INPUT);
 
   #Luz Verde
-  pinMode(verde, OUTPUT);
-  pinMode(verdeb, IMPUT);
+       pinMode(verde, OUTPUT);
+       pinMode(cartao & botao, INPUT);
 
+  
+}
+
+void loop() {
   
 }
